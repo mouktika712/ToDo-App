f03fdb44bc72ec77ecd966d98e965f665fcb36d7
Mouktika Namde <mouktikanamde712@gmail.com>
2022-05-30 17:19:16 -0700

    4th Commit: Install and Set-up the view engine (ejs)

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes/index'));
 
+// set-up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Something went wrong on the server side!`);
