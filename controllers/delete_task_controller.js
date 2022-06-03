32d2dc5366659a45b9717bce7c085265aee8b9aa
Mouktika Namde <mouktikanamde712@gmail.com>
2022-06-03 15:45:51 -0700

    11th Commit: Correct the delete_task_controller

--- controllers/delete_task_controller.js
+++ controllers/delete_task_controller.js
@@ -5,15 +5,12 @@ module.exports.delete_task = function (req, res) {
     const tasksToDelete = req.body.task;
 
     for(let i = 0; i < tasksToDelete.length; i++) {
-
-        taskSchema.deleteOne({
-            description: tasksToDelete[i]
-        }).then(function() {
-            console.log('deleted successfully!');
+        
+        taskSchema.deleteOne({description: tasksToDelete[i]}).then(function() {
+            console.log(`${tasksToDelete[i]} deleted successfully!`);
         }).catch(function (error) {
             console.log(error);
         });
-
     }
         
     console.log(req.body.task);
